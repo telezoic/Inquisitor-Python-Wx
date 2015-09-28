5ae5dbfce9301b7b6bd93910623a27ba57143a9a
telezoic <daniel.sifton@viu.ca>
2015-09-28 14:15:17 -0700

    gale update again

--- vendors/gale.py
+++ vendors/gale.py
@@ -8,11 +8,13 @@ for url in urls:
 	soup = BeautifulSoup(r.text)
 		
 	if soup.find_all(text = re.compile("Table of Contents")):
- 		print str(count) +  " of " + str(num_lines) + " | " + "Right On!" 
+ 		self.SetStatusText(str(count) +  " of " + str(num_lines) + " | " + "Right On!")
+ 		#print str(count) +  " of " + str(num_lines) + " | " + "Right On!" 
  		outurls.writerow([url[0], url[1], "Right On!"])
  		count += 1
  	else: 
-		print str(count) +  " of " + str(num_lines) + " | " + "Look into this . . . " + " | " + url[1]  + " | " + url[0]
+		self.SetStatusText(str(count) +  " of " + str(num_lines) + " | " + "Look into this . . . " + " | " + url[1]  + " | " + url[0])
+		#print str(count) +  " of " + str(num_lines) + " | " + "Look into this . . . " + " | " + url[1]  + " | " + url[0]
 		outurls.writerow([url[0], url[1], "Look into this . . ."])  
 		count += 1
 
