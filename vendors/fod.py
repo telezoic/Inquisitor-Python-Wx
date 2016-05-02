0de299b1af11fab89cb8f0a35c19ebf50e62a065
telezoic <daniel.sifton@viu.ca>
2016-05-02 14:18:51 -0700

    fod edits

--- vendors/fod.py
+++ vendors/fod.py
@@ -10,12 +10,13 @@ for url in urls:
 	soup = BeautifulSoup(r.text)
 	
 	
-	if soup.find_all("div", class_= "now-playing-div span-8"):
+	if soup.find_all("i", class_= "fa fa-play") or soup.find_all(text = re.compile("Show Segments")):
 		self.SetStatusText(str(count) +  " of " + str(num_lines) + " | " + "Right On!")
  		#print str(count) +  " of " + str(num_lines) + " | " + "Right On!" 
  		outurls.writerow([url[0], url[1], "Right On!"])
  		count += 1
- 		wx.Yield()
+ 		wx.Yield()	
+
 	elif soup.find_all("span", id = "MainContent_lblSearchTerm"): 
 		self.SetStatusText(str(count) +  " of " + str(num_lines) + " | " + "Nope!" + " | " + url[1] + " | " + url[0])
 		#print str(count) +  " of " + str(num_lines) + " | " + "Nope!" + " | " + url[1] + " | " + url[0]
